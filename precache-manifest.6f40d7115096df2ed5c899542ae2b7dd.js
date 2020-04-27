e8db25f0ec21fbe5b4303290f80685c17beebca3
canine89 <phk707kr@gmail.com>
2020-04-27 14:35:00 +0900

    Updates

---
+++ precache-manifest.6f40d7115096df2ed5c899542ae2b7dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b87d840b595ea9f8fcae31b8e2f58c3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d83fefd61e6f29c5c8d",
+    "url": "/static/css/main.3bc3c130.chunk.css"
+  },
+  {
+    "revision": "b38565b096a6fb8fbe3e",
+    "url": "/static/js/2.97519b23.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.97519b23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d83fefd61e6f29c5c8d",
+    "url": "/static/js/main.00be42ec.chunk.js"
+  },
+  {
+    "revision": "d99fd7440c811d59dc18",
+    "url": "/static/js/runtime-main.4e699483.js"
+  }
+]);
